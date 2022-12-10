14b1a0a33b06e23a26d690075e1a5ce91e70385e
Mark <miningmark48@gmail.com>
2022-12-09 23:24:34 -0500

    Update/cleanup features.py

--- util/features.py
+++ util/features.py
@@ -1,12 +1,12 @@
-import os.path as osp
-import yaml
 import logging
+import os.path as osp
 
+import yaml
 
-log = logging.getLogger("tidalbot")
-
+log = logging.getLogger("smiles")
 
 features_path = "features.yml"
+
 
 def get_extensions() -> list:
     """
@@ -25,24 +25,24 @@ def get_extensions() -> list:
             data = yaml.full_load(file)
 
             extensions = data["extensions"]
-            
+
             for e in extensions:
                 e_name = e["extension"]
 
                 if "directory" in e:
                     e_name = f"{e['directory']}.{e_name}"
-                
+
                 e_enabled = e["enabled"] if "enabled" in e else True
                 if e_enabled:
                     e_external = e["external"] if "external" in e else False
-                    
+
                     if e_external:
                         exts.append(e_name)
                         log.debug(f"Extension Found | External | {e_name}")
                     else:
                         exts.append(f"cogs.{e_name}")
                         log.debug(f"Extension Found | Internal | {e_name}")
-                    
+
                     # else:
                     #     exts.append(f"cogs.{category}.{e_name}")
                     #     log.debug(f"Extension Found | Cog | {category}.{e_name}")
@@ -51,6 +51,7 @@ def get_extensions() -> list:
     # log.debug(exts)
 
     return exts
+
 
 def get_commands_blacklist() -> list:
     """
@@ -69,18 +70,18 @@ def get_commands_blacklist() -> list:
             data = yaml.full_load(file)
 
             if not "commands" in data:
-                log.warn("Commands blacklist object not found in features.yml file")
-                return list() # Return empty list
+                log.warning("Commands blacklist object not found in features.yml file")
+                return list()  # Return empty list
 
             commands = data["commands"]
 
             if not commands or len(commands) == 0:
                 log.debug("Empty blacklist commands data, returning...")
-                return list() # Return empty list
-            
+                return list()  # Return empty list
+
             for c in commands:
                 c_name = c["command"]
-                
+
                 e_enabled = c["enabled"] if "enabled" in c else True
                 if not e_enabled:
                     cmds.append(c_name)
